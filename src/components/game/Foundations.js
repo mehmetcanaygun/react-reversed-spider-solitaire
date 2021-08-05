42d7c8d2f2d742a3961b152a2f12577c1942bb6a
mehmetcanaygun <mehmetcanaygun@gmail.com>
2021-08-05 12:57:56 +0300

    Add foundation slots and fill correct amount of them

--- src/components/game/Foundations.js
+++ src/components/game/Foundations.js
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useContext } from "react";
+import SolitaireContext from "../../context/solitaireContext";
 
 const Foundations = () => {
-  return <div className="foundations">Foundations</div>;
+  const solitaireContext = useContext(SolitaireContext);
+  const { foundations } = solitaireContext;
+
+  // Format Slot Class - Takes slot index, checks if slot index is less than foundations state, returns proper className
+  const formatSlotClass = (index) => {
+    return index <= foundations ? "foundation-slot filled" : "foundation-slot";
+  };
+
+  return (
+    <ul className="foundations">
+      <li className={formatSlotClass(8)}></li>
+      <li className={formatSlotClass(7)}></li>
+      <li className={formatSlotClass(6)}></li>
+      <li className={formatSlotClass(5)}></li>
+      <li className={formatSlotClass(4)}></li>
+      <li className={formatSlotClass(3)}></li>
+      <li className={formatSlotClass(2)}></li>
+      <li className={formatSlotClass(1)}></li>
+    </ul>
+  );
 };
 
 export default Foundations;
