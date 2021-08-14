7c64548c3d55c0d02ef49eaf9d2109d0bee6462b
mehmetcanaygun <mehmetcanaygun@gmail.com>
2021-08-14 23:04:09 +0300

    Add styles for End component

--- src/components/layout/End.js
+++ src/components/layout/End.js
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useContext } from "react";
+import SolitaireContext from "../../context/solitaireContext";
 
 const End = () => {
-  return <div className="end">Game End Container</div>;
+  const solitaireContext = useContext(SolitaireContext);
+  const { reset } = solitaireContext;
+
+  return (
+    <div className="end">
+      <div className="img-container">
+        <img src="/assets/end-spider.svg" alt="Spider" />
+      </div>
+
+      <p className="end-msg">Congratulations</p>
+
+      <p className="end-score">You've got 1000 points in 05:32</p>
+
+      <button className="again-btn" onClick={reset}>
+        Again
+      </button>
+    </div>
+  );
 };
 
 export default End;
