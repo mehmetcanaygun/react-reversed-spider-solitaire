903656940a9d467a198a21ed644800dea59b9632
mehmetcanaygun <mehmetcanaygun@gmail.com>
2021-08-16 18:23:07 +0300

    Redesign Start component

--- src/components/layout/Start.js
+++ src/components/layout/Start.js
@@ -13,11 +13,29 @@ const Start = () => {
         <h1 className="title">
           Welcome to <span>MCA's Spider Solitaire</span>
         </h1>
-        <p className="info">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquid
-          numquam optio quisquam aspernatur fuga illum doloremque laborum
-          maiores, quia dolorem.
-        </p>
+        <div className="info">
+          <p>
+            If you're familiar with Spider Solitaire you can get a grip on MCA's
+            Spider Solitaire in-a-flash!
+          </p>
+          <h2>Rules</h2>
+          <ul>
+            <li>
+              🕷️ To move the cards, firstly click on a card or cards, then click
+              on the destination card
+            </li>
+            <li>🕷️ Sort the cards from "Ace" to "King"</li>
+            <li>🕷️ You cannot click on the faced down cards</li>
+            <li>
+              🕷️ You cannot add cards from the stock if there's an empty pile
+            </li>
+            <li>🕷️ You will receive 125 points for each "A" to "K" run</li>
+            <li>
+              🕷️ Try to be the fastest MCA's Spider Solitaire player alive, and
+              enjoy
+            </li>
+          </ul>
+        </div>
         <button
           className="play-btn"
           onClick={() => {
@@ -30,6 +48,12 @@ const Start = () => {
         >
           Play
         </button>
+        <div className="spider">
+          <img
+            src="/assets/spider-color-tertiary-no-stroke.png"
+            alt="Loading"
+          />
+        </div>
       </div>
     </div>
   );
