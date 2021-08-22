362a0bbbd3591e276ccf860ac19f704ceefcb8e8
mehmetcanaygun <mehmetcanaygun@gmail.com>
2021-08-22 17:18:26 +0300

    Add Not Found page

--- src/components/pages/NotFound.js
+++ src/components/pages/NotFound.js
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useEffect, useContext } from "react";
+import { Link } from "react-router-dom";
+import SolitaireContext from "../../context/solitaireContext";
 
 const NotFound = () => {
-  return <main className="not-found-page">Not Found Page</main>;
+  const solitaireContext = useContext(SolitaireContext);
+  const { reset } = solitaireContext;
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+    reset();
+
+    // eslint-disable-next-line
+  }, []);
+
+  return (
+    <main className="not-found-page">
+      <div className="not-found-container">
+        <p className="code">404</p>
+        <p className="text">The page you're looking for could not be found.</p>
+        <Link to="/" className="back-to-home-link">
+          Crawl Back To Home
+        </Link>
+      </div>
+    </main>
+  );
 };
 
 export default NotFound;
